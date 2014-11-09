351af40e6c8cafbff5c946eebd63752fe9e1f0d3
Kevin Buchanan <kevaustinbuch@gmail.com>
2014-11-09 01:32:28 +0000

    refacto coin changer

--- src/coin_change.erl
+++ src/coin_change.erl
@@ -2,14 +2,10 @@
 -export([change/1
         ]).
 
-take_amount(Amount, Coin) ->
-    Taken = Amount div Coin,
-    {Amount - Taken * Coin, Taken}.
-
 change(Amount) ->
-    {Five, Dollars} = take_amount(Amount, 100),
-    {Four, Quarters} = take_amount(Five, 25),
-    {Three, Dimes} = take_amount(Four, 10),
-    {Two, Nickels} = take_amount(Three, 5),
-    {_, Pennies} = take_amount(Two, 1),
-    {Pennies, Nickels, Dimes, Quarters, Dollars}.
+    TakeAmount = fun(Coin, {Remaining, Coins}) ->
+        Taken = Remaining div Coin,
+        {Remaining - Taken * Coin, lists:append(Coins, [Taken])}
+    end,
+    {_, Change} = lists:foldl(TakeAmount, {Amount, []}, [100, 25, 10, 5, 1]),
+    lists:reverse(Change).
